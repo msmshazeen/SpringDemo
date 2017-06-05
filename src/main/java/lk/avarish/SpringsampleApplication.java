117ea90a14ae09b11768082e223a9fd304819643
Thisaru Mahadurage <thisarumahadurage@Thisarus-MacBook-Air.local>
2017-06-05 09:36:27 +0530

    First Commit

---
+++ src/main/java/lk/avarish/SpringsampleApplication.java
@@ -0,0 +1,12 @@
+package lk.avarish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsampleApplication.class, args);
+	}
+}
---
+++ src/test/java/lk/avarish/SpringsampleApplicationTests.java
@@ -0,0 +1,16 @@
+package lk.avarish;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringsampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
