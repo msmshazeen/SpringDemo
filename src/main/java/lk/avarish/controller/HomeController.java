ee78a0e73e7cf4039ca26c5f0fc9cba8b2312ff1
Avishka Perera <avishkaperera4@gmail.com>
2017-06-05 11:51:40 +0530

    added home controller

---
+++ src/main/java/lk/avarish/controller/HomeController.java
@@ -0,0 +1,12 @@
+package lk.avarish.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String Home(){
+        return "index";
+    }
+}
--- src/main/java/lk/avarish/data/model/Role.java
+++ src/main/java/lk/avarish/data/model/Role.java
@@ -1,8 +1,32 @@
 package lk.avarish.data.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Role {
     private int id;
     private String roleName;
-    private String lastName;
-    private String
+
+    public Role(){}
+
+    public Role(int id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
