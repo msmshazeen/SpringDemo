2ecfbf871ce90014c2560fe86bdba02e57a62f76
thisarumahadurage <thisaru@gmx.com>
2017-06-05 14:38:51 +0530

    Views Complete (excluded : Registration)

--- src/main/java/lk/avarish/security/SecurityConfig.java
+++ src/main/java/lk/avarish/security/SecurityConfig.java
@@ -1,16 +1,47 @@
 package lk.avarish.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
                 .authorizeRequests()
-                .antMatchers("/","/index").permitAll();
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username").passwordParameter("password")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+
+//        auth
+//                .jdbcAuthentication().dataSource(dataSource)
+//                .usersByUsernameQuery("SELECT username, password, active FROM users WHERE username = ?")
+//                .authoritiesByUsernameQuery("SELECT username, role FROM users WHERE username = ?");
+
     }
 }
